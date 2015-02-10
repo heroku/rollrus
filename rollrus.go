b8e0d01fef010d7ee028cbba8d80afd236bdb88a
Edward Muller <edwardam@interlix.com>
2015-02-10 10:53:31 -0800

    Use the sync rollbar client instead

--- rollrus.go
+++ rollrus.go
@@ -2,35 +2,55 @@ package rollrus
 
 import (
 	"fmt"
+	"time"
 
 	log "github.com/Sirupsen/logrus"
-	"github.com/heroku/rollbar"
+	"github.com/stvp/roll"
 )
 
-var levelToRollbar = map[log.Level]string{
-	log.ErrorLevel: rollbar.ERR,
-	log.FatalLevel: rollbar.CRIT,
-	log.PanicLevel: rollbar.CRIT,
-}
-
 type Hook struct {
-	Client rollbar.Client
+	Client roll.Client
 }
 
-func (r *Hook) Fire(entry *log.Entry) error {
-	if level, exists := levelToRollbar[entry.Level]; !exists {
-		r.Client.Error(rollbar.ERR, fmt.Errorf(entry.Message))
-	} else {
-		r.Client.Error(level, fmt.Errorf(entry.Message))
+func (r *Hook) Fire(entry *log.Entry) (err error) {
+	e := fmt.Errorf(entry.Message)
+	m := convertFields(entry.Data)
+	if _, exists := m["time"]; !exists {
+		m["time"] = entry.Time.Format(time.RFC3339)
 	}
 
-	return nil
+	switch entry.Level {
+	case log.FatalLevel, log.PanicLevel:
+		_, err = r.Client.Critical(e, m)
+	case log.ErrorLevel:
+		_, err = r.Client.Error(e, m)
+	case log.WarnLevel:
+		_, err = r.Client.Warning(e, m)
+	case log.InfoLevel:
+		_, err = r.Client.Info(entry.Message, m)
+	case log.DebugLevel:
+		_, err = r.Client.Debug(entry.Message, m)
+	default:
+		return fmt.Errorf("Unknown level: %s", entry.Level)
+	}
+
+	return err
 }
 
-func (r *Hook) Levels() []log.Level {
-	return []log.Level{
-		log.ErrorLevel,
-		log.FatalLevel,
-		log.PanicLevel,
+func convertFields(fields log.Fields) map[string]string {
+	m := make(map[string]string)
+	for k, v := range fields {
+		switch t := v.(type) {
+		case time.Time:
+			m[k] = t.Format(time.RFC3339)
+		default:
+			if s, ok := v.(fmt.Stringer); ok {
+				m[k] = s.String()
+			} else {
+				m[k] = fmt.Sprintf("%+v", t)
+			}
+		}
 	}
+
+	return m
 }
