d7a739e216d9b4ff41aa2c9d3625b76f05f6cb8a
Josh W Lewis <josh.w.lewis@gmail.com>
2016-08-15 17:48:46 -0500

    Move usage from README to real examples

--- rollrus_test.go
+++ rollrus_test.go
@@ -6,12 +6,35 @@ import (
 	"testing"
 	"time"
 
-	log "github.com/Sirupsen/logrus"
+	"github.com/Sirupsen/logrus"
 	"github.com/stvp/roll"
 )
 
+
+func ExampleSetupLogging() {
+  SetupLogging("some-long-token", "staging")
+
+  // This will not be reported to Rollbar
+	logrus.Info("OHAI")
+
+  // This will be reported to Rollbar
+	logrus.WithFields(logrus.Fields{"hi":"there"}).Fatal("The end.")
+}
+
+func ExampleNewHook() {
+  log := logrus.New()
+  hook := NewHook("my-secret-token", "production")
+  log.Hooks.Add(hook)
+
+  // This will not be reported to Rollbar
+	log.WithFields(logrus.Fields{"power_level": "9001"}).Debug("It's over 9000!")
+
+  // This will be reported to Rollbar
+  log.Panic("Boom.")
+}
+
 func TestIntConversion(t *testing.T) {
-	i := make(log.Fields)
+	i := make(logrus.Fields)
 	i["test"] = 5
 
 	r := convertFields(i)
@@ -27,7 +50,7 @@ func TestIntConversion(t *testing.T) {
 }
 
 func TestErrConversion(t *testing.T) {
-	i := make(log.Fields)
+	i := make(logrus.Fields)
 	i["test"] = fmt.Errorf("This is an error")
 
 	r := convertFields(i)
@@ -43,7 +66,7 @@ func TestErrConversion(t *testing.T) {
 }
 
 func TestStringConversion(t *testing.T) {
-	i := make(log.Fields)
+	i := make(logrus.Fields)
 	i["test"] = "This is a string"
 
 	r := convertFields(i)
@@ -60,7 +83,7 @@ func TestStringConversion(t *testing.T) {
 
 func TestTimeConversion(t *testing.T) {
 	now := time.Now()
-	i := make(log.Fields)
+	i := make(logrus.Fields)
 	i["test"] = now
 
 	r := convertFields(i)
@@ -82,7 +105,7 @@ func TestTriggerLevels(t *testing.T) {
 		t.Fatal("Expected Levels() to return defaultTriggerLevels")
 	}
 
-	newLevels := []log.Level{log.InfoLevel}
+	newLevels := []logrus.Level{logrus.InfoLevel}
 	underTest.triggers = newLevels
 	if !reflect.DeepEqual(underTest.Levels(), newLevels) {
 		t.Fatal("Expected Levels() to return newLevels")
